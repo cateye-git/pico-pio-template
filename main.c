645f8f7bfb0c990aaad41784ec54a22722e22ddb
cateye-git <ashley_github@crossleys.biz>
2023-04-22 15:15:30 +0100

    improve code formatting

--- main.c
+++ main.c
@@ -7,9 +7,9 @@
 #include "ledcontrol.pio.h"
 
 
-int main() {
+int main () {
     // initialise the `stdio` library so that we can use printf() etc
-    stdio_init_all();
+    stdio_init_all ();
 
     // decide which PIO we want to use (the RP2040 has two of them)
     PIO pio = pio0;
@@ -18,19 +18,19 @@ int main() {
     uint offset = pio_add_program (pio, &ledcontrol_program);
 
     // claim a free state machine on the PIO
-    uint sm = pio_claim_unused_sm(pio, true);
+    uint sm = pio_claim_unused_sm (pio, true);
 
     // use our helper function to configure and start the state machine
-    ledcontrol_init(pio, sm, offset, PICO_DEFAULT_LED_PIN, 1e6);
+    ledcontrol_init (pio, sm, offset, PICO_DEFAULT_LED_PIN, 1e6);
 
     // control the LED by sending data to the state machine's TX FIFO
     while (true) {
         puts ("LED on");
-        pio_sm_put_blocking(pio, sm, 1);
-        sleep_ms(500);
+        pio_sm_put_blocking (pio, sm, 1);
+        sleep_ms (500);
 
         puts ("LED off");
-        pio_sm_put_blocking(pio, sm, 0);
-        sleep_ms(1000);
+        pio_sm_put_blocking (pio, sm, 0);
+        sleep_ms (1000);
     }
 }
